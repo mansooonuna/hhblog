affb9ca0312646a46acd57219ddbbb8294f7be80
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-04-15 10:54:29 +0900

    first commit

---
+++ src/main/java/com/sparta/hhblog/HhblogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hhblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhblogApplication.class, args);
+    }
+
+}
